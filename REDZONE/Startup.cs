b43891c2e41e1580cbf57e85fd638f3d3f5b090c
FELICIANO DELGADO <feliciano.delgado@dsc-logistics.com>
2016-05-31 16:55:21 -0500

    New redzone project 5/31

---
+++ REDZONE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(REDZONE.Startup))]
+namespace REDZONE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
