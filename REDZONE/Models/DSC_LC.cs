e3779413f1b8fd690282710373f3cfca3d3c6a5d
FELICIANO DELGADO <feliciano.delgado@dsc-logistics.com>
2016-06-02 13:51:33 -0500

    test

---
+++ REDZONE/Models/DSC_LC.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace REDZONE.Models
+{
+    public class DSC_LC
+    {
+        public int dsc_lc_id { set; get; }
+    }
+}
