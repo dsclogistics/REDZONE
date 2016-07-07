18257634757aebacaf07ff2d15b5ddbe88872127
FELICIANO DELGADO <feliciano.delgado@dsc-logistics.com>
2016-07-07 10:17:27 -0500

    .

---
+++ REDZONE/Controllers/ExecutiveSummaryController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace REDZONE.Controllers
+{
+    public class ExecutiveSummaryController : Controller
+    {
+        // GET: ExecutiveSummary/MonthlySummary
+        public ActionResult MonthlySummary(string rptMonth)
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ REDZONE/Models/ExecutiveSummaryView.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace REDZONE.Models
+{
+    public class ExecutiveSummaryView
+    {
+        public string month { get; set; }
+        MetricEntity goal = new MetricEntity();
+        MetricEntity redTotals = new MetricEntity();
+        List<MetricEntity> buildings = new List<MetricEntity>();
+
+        //-------- Empty Constructor -----------
+        public ExecutiveSummaryView() { }
+        //-------- End of Constructor ----------
+
+    }
+
+    public class MetricEntity
+    {
+        public string Name { get; set; }
+        public string score { get; set; }
+        public string scoreColor { get; set; }
+        public List<MeasuredMetric> entityMetrics = new List<MeasuredMetric>();        
+
+        //-------- Empty Constructor -----------
+        public MetricEntity() { }
+        //-------- End of Constructor ----------
+    }
+
+    public class MeasuredMetric{
+        public string metricName { get; set; }
+        public string metricValue { get; set; }
+        public string metricColor { get; set; }
+        //-------- Empty Constructor -----------
+        public MeasuredMetric() { }
+        //-------- End of Constructor ----------
+    }
+}
