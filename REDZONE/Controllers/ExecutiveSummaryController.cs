2721806cd9b821ad25ea78a96eea4306eee6d1ff
FELICIANO DELGADO <feliciano.delgado@dsc-logistics.com>
2016-07-20 10:50:52 -0500

    created dashboard front end

--- REDZONE/Controllers/ExecutiveSummaryController.cs
+++ REDZONE/Controllers/ExecutiveSummaryController.cs
@@ -12,17 +12,30 @@ namespace REDZONE.Controllers
         // GET: ExecutiveSummary/MonthlySummary
         public ActionResult MonthlySummary(string rptMonth)
         {
-            ExecutiveSummaryViewModel testDashboard = new ExecutiveSummaryViewModel();
-
-            testDashboard = dummyData();
-            return View(testDashboard);
+            return View(testDashboard());
         }
 
-        private ExecutiveSummaryViewModel dummyData()
+        private ExecutiveSummaryViewModel testDashboard()
         {
             ExecutiveSummaryViewModel temp = new ExecutiveSummaryViewModel();
 
+            // Load the Metric Header Info
             temp.month = "June";
+            temp.year = "2016";
+            temp.urlNextMonth = "";
+            temp.urlPrevMonth = "";
+            temp.statusNextMonth = "disabled";
+            temp.statusPrevMonth = "disabled";
+            // Load the Goals Row (Use dummy values)
+            temp.goal = new BuildingMetricEntity("GOAL");
+            //Load two sample building Rows
+            BuildingMetricEntity sampleBuilding = new BuildingMetricEntity("AP");
+            temp.buildings.Add(sampleBuilding);
+            sampleBuilding = new BuildingMetricEntity("PP");
+            temp.buildings.Add(sampleBuilding);
+            sampleBuilding = new BuildingMetricEntity("PC");
+            temp.buildings.Add(sampleBuilding);
+
             return temp;
         }
     }
