a9d56cc30c5b4f550a6985010a503599e54d5a1f
Alex Chen <alex.chen@dsc-logistics.com>
2016-07-21 11:33:25 -0500

    ..

--- REDZONE/AppCode/APIDataParcer.cs
+++ REDZONE/AppCode/APIDataParcer.cs
@@ -238,6 +238,54 @@ namespace REDZONE.AppCode
             }
             return monthNo;
         }
+
+        public static string intToMonth(int monthNo)
+        {
+            string monthName = "";
+            switch (monthNo)
+            {
+                case 1:
+                    monthName = "January";
+                    break;
+                case 2:
+                    monthName = "February";
+                    break;
+                case 3:
+                    monthName = "March";
+                    break;
+                case 4:
+                    monthName = "April";
+                    break;
+                case 5:
+                    monthName = "May";
+                    break;
+                case 6:
+                    monthName = "June";
+                    break;
+                case 7:
+                    monthName = "July";
+                    break;
+                case 8:
+                    monthName = "August";
+                    break;
+                case 9:
+                    monthName = "September";
+                    break;
+                case 10:
+                    monthName = "October";
+                    break;
+                case 11:
+                    monthName = "November";
+                    break;
+                case 12:
+                    monthName = "December";
+                    break;
+                default:
+                    break;
+            }
+            return monthName;
+        }
+
         public ExecutiveSummaryViewModel getExcecutiveSummaryView(int metric_id, string month, string year)
         {
             ExecutiveSummaryViewModel eSummary = new ExecutiveSummaryViewModel();          
