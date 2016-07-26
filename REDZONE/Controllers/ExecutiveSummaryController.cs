1edbec953d0ec0b6070bb2ffb165ebd0e6e35a77
FELICIANO DELGADO <feliciano.delgado@dsc-logistics.com>
2016-07-26 11:38:58 -0500

    added selected style for selected menu

--- REDZONE/Controllers/ExecutiveSummaryController.cs
+++ REDZONE/Controllers/ExecutiveSummaryController.cs
@@ -26,21 +26,5 @@ namespace REDZONE.Controllers
 
             return View(dashBoard);
         }
-
-        // GET: ExecutiveSummary/BuildingSummary
-        public ActionResult BuildingSummary()
-        {
-
- 
-            return View();
-        }
-
-        // GET: ExecutiveSummary/MetricSummary
-        public ActionResult MetricSummary()
-        {
-
-
-            return View();
-        }
     }
 }
