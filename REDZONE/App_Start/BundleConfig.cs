a7355cf1c6f13f9411df6f8e4f5e282a98ced5aa
FELICIANO DELGADO <feliciano.delgado@dsc-logistics.com>
2016-08-18 09:53:50 -0500

    .

--- REDZONE/App_Start/BundleConfig.cs
+++ REDZONE/App_Start/BundleConfig.cs
@@ -8,15 +8,9 @@ namespace REDZONE
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                    "~/Content/bootstrap.css",
-                    "~/Content/site.css"
-            ));
-
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                     //"~/Scripts/jquery-{version}.js",
-                    "~/Scripts/jquery.js",
-                    "~/Scripts/jquery-ui.js"
+                    "~/Scripts/jquery.js"
             ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
@@ -29,12 +23,17 @@ namespace REDZONE
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"
-            ));
+                      "~/Scripts/respond.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryUI").Include(
                       "~/Scripts/jquery-ui-{version}.js" 
             ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"
+));
+
         }
     }
 }
