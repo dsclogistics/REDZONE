6cf76a7b872a6a45d8393f98baf51d5d8dcb1687
FELICIANO DELGADO <feliciano.delgado@dsc-logistics.com>
2016-08-18 11:56:46 -0500

    .

--- REDZONE/App_Start/BundleConfig.cs
+++ REDZONE/App_Start/BundleConfig.cs
@@ -9,8 +9,8 @@ namespace REDZONE
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                    //"~/Scripts/jquery-{version}.js",
-                    "~/Scripts/jquery.js"
+                        //"~/Scripts/jquery-{version}.js"
+                        "~/Scripts/jquery.js"
             ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
