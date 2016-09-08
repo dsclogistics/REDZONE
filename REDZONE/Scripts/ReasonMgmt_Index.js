16f690a2471f5b9b948af554c56144a79800f828
FELICIANO DELGADO <feliciano.delgado@dsc-logistics.com>
2016-09-08 13:06:08 -0500

    .

--- REDZONE/Scripts/ReasonMgmt_Index.js
+++ REDZONE/Scripts/ReasonMgmt_Index.js
@@ -3,6 +3,8 @@ var fixHelper = function (e, ui) {
     ui.children().each(function () {
         $(this).width($(this).width());
     });
+    ui.css("color", "blue");
+    ui.css("font-weight", "bold");
     return ui;
 };
 
@@ -12,16 +14,26 @@ function reasonSortEnable() {
     {
         $("#sortTable tbody").sortable('enable');
     }
-    //$("#sortTable tbody").sortable('enable');
     $("#sortTable tbody").sortable({
-        placeholder: 'ui-state-highlight',
-        helper: fixHelper            //used to prevent the columns from collapsing while dragging the row
+        placeholder: 'sort-state-highlight',
+        helper: fixHelper,            //used to prevent the columns from collapsing while dragging the row
+        //items: "tr:not(.sort-state-disabled)"
+        update: function (event, ui) {
+            //Reset the look and feel of the dragged row
+            ui.item.css("color", "black");
+            ui.item.css("font-weight", "normal");
+            //alert("Item was sorted.");
+        }
     }).disableSelection();
 }
 
 //Disable the table sortability capabilities
 function reasonSortDisable() {
     $("#sortTable tbody").sortable('disable');
+    //Perform the removal of the UI styling 
+    $('.reasonListRow').removeClass('sortableRow');
+    $('.firstCol').css("border-radius", "0px");
+    $('.lastCol').css("border-radius", "0px");
 }
 
 
