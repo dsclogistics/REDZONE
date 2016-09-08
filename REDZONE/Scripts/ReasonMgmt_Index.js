ee1051b62102fe6a8ef76ce09d47c99f7cc03de1
FELICIANO DELGADO <feliciano.delgado@dsc-logistics.com>
2016-09-08 11:25:04 -0500

    .

---
+++ REDZONE/Scripts/ReasonMgmt_Index.js
@@ -0,0 +1,32 @@
+﻿//alert("Javascript file is connected");
+var fixHelper = function (e, ui) {
+    ui.children().each(function () {
+        $(this).width($(this).width());
+    });
+    return ui;
+};
+
+//Enable the table sortability capabilities
+function reasonSortEnable() {
+    if ($("#sortTable tbody").sortable())
+    {
+        $("#sortTable tbody").sortable('enable');
+    }
+    //$("#sortTable tbody").sortable('enable');
+    $("#sortTable tbody").sortable({
+        placeholder: 'ui-state-highlight',
+        helper: fixHelper            //used to prevent the columns from collapsing while dragging the row
+    }).disableSelection();
+}
+
+//Disable the table sortability capabilities
+function reasonSortDisable() {
+    $("#sortTable tbody").sortable('disable');
+}
+
+
+$(document).ready(function () {
+
+
+
+});
